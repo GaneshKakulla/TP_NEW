d24226a6a16ec8fb91f9dd62f59fafe9e1b2c100
hp <hp@GANESH>
2025-01-17 17:42:53 +0530

    Add project files.

---
+++ TP_NEW/Controllers/UserDetailsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP_NEW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hi how are you");
+        }
+    }
+}
